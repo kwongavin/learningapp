5a70687f62ca6da593e70a0f2d6c91fac7b287b6
gavin <kwongavin@icloud.com>
2023-03-05 22:20:18 -0800

    Initial Commit

---
+++ (Gavin) Learning App/_Gavin__Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Gavin__Learning_AppApp.swift
+//  (Gavin) Learning App
+//
+//  Created by Gavin Kwon on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct _Gavin__Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
